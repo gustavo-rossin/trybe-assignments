cf095d7bd199d3495956a02abb851b46fd84dd9d
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-07-21 16:32:02 -0300

    added html for the example

--- foundations/04-Introduction-JavaScript/01-First-Steps/scripts_01.js
+++ foundations/04-Introduction-JavaScript/01-First-Steps/scripts_01.js
@@ -1,17 +1,68 @@
-// Variáveis e Constantes
+// // Variáveis e Constantes
 
-const myName = "Gustavo";
-const birthCity = "Campinas";
-let birthYear = 20322;
+// const myName = "Gustavo";
+// const birthCity = "Campinas";
+// let birthYear = 20322;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
 
-// TEM QUE SALVAR ANTES DE DAR O RUN CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-// Tipos primitivos, tipagem dinâmica e operações aritméticas
+// // TEM QUE SALVAR ANTES DE DAR O RUN CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-let movie = "Avengers"
+// // Tipos primitivos, tipagem dinâmica e operações aritméticas
+
+
+// let movie = "Avengers"; //string literal
+// let score = 10.91; //number literal
+// let isValid = true; //boolean
+// let name; // undefined
+// let color = null; // redefinição de valor
+
+// console.log(movie, score, isValid, name, color)
+
+
+// // CONTAS ARITMÉTICAS +, -, *, /, **
+
+// salary = 3500;
+
+// // console.log(salary + salary);
+// // console.log(salary - salary);
+// // console.log(salary / salary);
+// // console.log(2 * 200);
+// // console.log(3 ** 3);
+
+// salary++; //ENCREMENTAR (++) EM 1 OU DECREMENTAR (--) EM 1.
+
+// console.log(salary)
+
+
+// PARTE EXERCICIOS DA AULA
+
+// tipos de variáveis
+
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge);
+
+// tipos de aritmetica
+
+let base = 5;
+let height = 8;
+const area = base*height;
+const perimeter = base*2 + height*2
+
+
+console.log(area);
+console.log(perimeter);
