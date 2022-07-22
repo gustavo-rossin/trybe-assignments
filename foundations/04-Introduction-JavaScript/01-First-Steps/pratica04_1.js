f5363b0d3e6297d47c3eb8fcac75bdaca9505698
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-07-22 12:59:21 -0300

    Exercises 1 until 8 done.

---
+++ foundations/04-Introduction-JavaScript/01-First-Steps/pratica04_1.js
@@ -0,0 +1,146 @@
+
+const a = 10
+const b = 12
+
+// 1)
+// adicao = a + b;
+// subtracao = a - b;
+// multiplicacao = a * b;
+// divisao = a/b;
+// resto = a % b;
+
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(resto);
+
+// 2 && 3
+
+const c = 23;
+
+// if (a > b && a > c) {
+//     console.log (a)
+// }
+
+// if (b > a && b > c) {
+//     console.log (b)
+// }
+// else {
+//     console.log (c)
+// }
+
+// 4
+
+// const d = -1;
+// messageNumber = "";
+
+// if (d > 0) {
+//     messageNumber = "Positive"
+//     console.log(messageNumber);
+// }
+// else if (d === 0) {
+//     messageNumber = "Zero"
+//     console.log(messageNumber);
+// }
+
+// else {
+//     messageNumber = "Negative"
+//     console.log(messageNumber);
+// }
+
+// 5
+
+// ladoA = 90;
+// ladoB = 45;
+// ladoC = -45;
+// msgTriangle = "";
+// isTriangle = ladoA + ladoB + ladoC
+
+// if (isTriangle === 180 && ladoA > 0 && ladoB > 0 && ladoC > 0) {
+// msgTriangle = "É triangulo"
+// console.log(msgTriangle);
+// }
+
+// else {
+//     msgTriangle = "Não É!!!!!!!!"
+// console.log(msgTriangle)
+// }
+
+// 6
+
+// let chessMovement = 'RoOk'
+// let lowerCase = chessMovement.toLowerCase()
+
+// switch (lowerCase) {
+//     case 'bishop':
+//         console.log('Diagonal');
+//         break
+//     case 'queen':
+//         console.log('Any direction');
+//         break
+//     case 'knight': // cavalo
+//         console.log('in L');
+//         break
+//     case 'rook': // torre
+//         console.log('Straight lines');
+//         break
+    
+// }
+
+// 7
+
+// gradeStudent = 66;
+// messageGrade = '';
+
+// if (gradeStudent >= 90 && gradeStudent <= 100) {
+//     messageGrade = 'tirou nota A';
+//     console.log(messageGrade);
+// }
+
+// else if (gradeStudent >= 80 && gradeStudent < 90) {
+//     messageGrade = 'tirou nota B';
+//     console.log(messageGrade);
+// }
+
+// else if (gradeStudent >= 70 && gradeStudent < 80) {
+//     messageGrade = 'tirou nota C';
+//     console.log(messageGrade);
+// }
+
+// else if (gradeStudent >= 60 && gradeStudent < 70) {
+//     messageGrade = 'tirou nota D';
+//     console.log(messageGrade);
+// }
+
+// else if (gradeStudent >= 50 && gradeStudent < 60) {
+//     messageGrade = 'tirou nota E';
+//     console.log(messageGrade);
+// }
+
+// else if (gradeStudent >= 0 && gradeStudent < 50) {
+//     messageGrade = 'tirou nota F';
+//     console.log(messageGrade);
+// }
+
+// else {
+//     messageGrade = 'ERROOOOOOOOOOOOOOO'
+//     console.log(messageGrade)
+// }
+
+// 8
+
+const numberOne = 1;
+const numberTwo = 21;
+const numberThree = 3;
+
+messageExercise = '';
+
+if (numberOne%2 === 0 || numberTwo%2 === 0 || numberThree%2 === 0) {
+    messageExercise = 'O número é par'
+    console.log(messageExercise)
+}
+else {
+    messageExercise = 'Só tem número ímpar'
+    console.log(messageExercise)
+}
