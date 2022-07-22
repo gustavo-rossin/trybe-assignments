a1de652f4c4319ba344d078256cdb03a43ced630
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-07-22 16:08:25 -0300

    moved file to the right directory

---
+++ foundations/04-Introduction-JavaScript/02-Array-and-Loop-For/scripts_02.js
@@ -0,0 +1,14 @@
+let pizza1 = '4 queijos';
+let pizza2 = 'Frango com catupiry';
+let pizza3 = 'marguerita';
+let pizza4 = 'palmito';
+let pizza5 = 'chocolate';
+
+const cardapio = [pizza1, pizza2, pizza3, pizza4, pizza5];
+
+console.log(cardapio);
+
+let pizzas = ['4 queijos', 'frango com catupiry', 'marguerita', 'palmito', 'chocolate'];
+
+
+console.log(pizzas)
