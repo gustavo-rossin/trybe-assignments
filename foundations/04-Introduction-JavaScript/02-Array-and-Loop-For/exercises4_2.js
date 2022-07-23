ae81fe8176251a074042be5b38e2c9ec4a016224
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-07-23 20:59:52 -0300

    finished all 9 exercises in 4.2 lecture

--- foundations/04-Introduction-JavaScript/02-Array-and-Loop-For/exercises4_2.js
+++ foundations/04-Introduction-JavaScript/02-Array-and-Loop-For/exercises4_2.js
@@ -45,16 +45,50 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(maior)
 
-// 6
+// // 6
 
-let numerosImpares = 0;
+// let numerosImpares = 0;
 
-for (i = 0; i < numbers.length; i += 1) {
+// for (i = 0; i < numbers.length; i += 1) {
 
-    if(numbers[i]%2 === 1) {
-        numerosImpares += 1
-        console.log(numbers[i])
-    }
+//     if(numbers[i]%2 === 1) {
+//         numerosImpares += 1
+//         console.log(numbers[i])
+//     }
+
+// }
+// console.log("Total de números ímpares:",numerosImpares)
+
+// 7
+
+// menorValor = 10000;
+
+// for (i=0; i<numbers.length; i+=1) {
+//     if(numbers[i] < menorValor) {
+//         menorValor = numbers[i]
+//         console.log(menorValor)
+//     }
+// }
+
+// console.log(menorValor)
+
+// 8 && 9
+
+valorAdicionado = 0;
+valorArray = [];
+divisao = [];
+
+for (index = 1; index <= 25; index += 1) {
+    valorAdicionado += 1
+    valorArray.push(valorAdicionado)
+    //console.log(valorArray)
+    
+}
+console.log(valorArray)
+
+for (c = 0; c < valorArray.length; c += 1) {
+    divisao.push(valorArray[c]/2)
+    //console.log(divisao) 
 
 }
-console.log("Total de números ímpares:",numerosImpares)
+console.log(divisao) 
