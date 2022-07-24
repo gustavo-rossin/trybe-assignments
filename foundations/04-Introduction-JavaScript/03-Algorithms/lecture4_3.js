25d0bc0e709f264417ab8ce65b1cfe22341ebec7
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-07-23 23:35:53 -0300

    finished all lecture exercises

---
+++ foundations/04-Introduction-JavaScript/03-Algorithms/lecture4_3.js
@@ -0,0 +1,70 @@
+// let fruits = [3, 4, 10, 1, 12];
+// let soma = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+
+//     soma += fruits[index]
+//     //console.log(soma)
+
+// }
+// console.log(soma)
+
+// if (soma > 15) {
+
+//     console.log("VALOR ACIMA DE 15. O VALOR É:", soma)
+
+// }
+
+
+// const n = 9;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+//   console.log(resultado)
+// };
+// console.log(resultado);
+
+
+// a = 5;
+// b = 7;
+// c = 3;
+// d = 9;
+
+// console.log(a != 3 || b < 10 || c == 5)
+
+
+// fixando = 100;
+// somando = 0;
+
+// for (index = 1; index <= fixando; index += 1) {
+
+//     somando += index
+//     console.log(somando)
+
+// }
+// console.log(somando)
+
+// word = 'Trybe';
+// newWord = '';
+
+// //console.log(word[word.length-1]) -- pegar a última letra de trás para frente
+// for (index = 1; index <= word.length; index += 1) {
+
+// newWord += word[word.length-[index]]
+// //console.log(newWord)
+    
+// }
+// console.log(newWord)
+
+count = 0;
+
+for (let index = 1; index <= 100; index += 1) {
+
+    if (index%5 === 0) {
+        count += 1
+        console.log(count)
+    }
+
+
+}
