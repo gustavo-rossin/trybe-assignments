9d152fc30ba1c8e3140b1ec8f5238caddbb242b2
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-08-01 21:47:22 -0300

    finished lecture 5.4

---
+++ foundations/05-DOM-Events-WebStorage/04-JS-Web-Storage/lecture5_4.js
@@ -0,0 +1,29 @@
+// VID 1
+
+// DIFERENÇA ENTRE SESSION E LOCAL STORAGE:
+// Cookies - informações enviadas ao servidor
+// localStorage - fica salvo eternamente no seu pc. Informações retidas no pc.
+// SessionStorage - fica salvo até fechar o navegador. Informações retidas no pc.
+
+// localStorage.setItem('name', 'Gustavo Rossin') - declarar um elemento e sua variável.
+// localStorage.getItem('name') -  para buscar a variável que está em 'name'
+// localStorage.key(0) - mostra o elemento de índice zero. No caso o 'name'.
+// localStorage['nome'] - retorna a variável neste caso - 'Gustavo Rossin'.
+// localStorage.value(0) - retorna o valor da variável igual localStorage['nome']
+// localStorage - traz toda a informação
+// localStorage.removeItem('name') - exclui todo o elemento 'name'
+// delete localStorage.name - exclui o elemento 'name'
+//localStorage.clear() - remove TUDO de localStorage
+// JSON.stringify() - converte outros formatos de dado em string.
+// sessionStorage.getItem() - recupera um item do sessionStorage.
+
+
+
+// podemos utilizar laço de repetição
+// for (let index = 0; index < localStorage.length; index += 1) {
+    // let chave = localStorage.key(index);
+    // console.log(chave + ': ' + localStorage[chave]);
+// }
+
+
+
